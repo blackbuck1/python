2b83cae50403e68693969dce017872a39a80d3cf
Nihal Singh Verma <nihalsinghverma@hotmail.com>
2020-06-17 20:16:26 +0530

    Update and rename bonus.py to freight_bonus.py

--- freight_bonus.py
+++ freight_bonus.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Required library
-
 import pandas as pd
 import numpy as np
 import mysql.connector
 
+# Sqlconnector for DbConnection
 con = mysql.connector.connect(user='public', password='*******', host='*******', database='db_name')
+#Sqlquery for data
 query = "SELECT ROW_NUMBER() OVER(ORDER BY month, order_id ASC) AS id, a.* FROM zlog.order_details a WHERE customer_classification='Corporate' and registration_number <> '' "
 df = pd.read_sql(query, con=con)
 df.head(5)
 
+#Rank for first transacted truck
 df['rank'] = df.groupby('registration_number')['id'].rank(ascending=True)
 fdf = df[df['rank'] == 1]
 corp_acq_cost = fdf[['order_id', 'month', 'rank']]
 corp_acq_cost['bonus'] = np.where(corp_acq_cost['month'] == 1, 500,2000)
-corp_acq_cost.to_csv('Corporate Truck Acquisition Cost.csv',index=False)
+corp_acq_cost.to_csv('Corporate Truck Acquisition Cost.csv', index=False)
 corp_acq_cost.head()
